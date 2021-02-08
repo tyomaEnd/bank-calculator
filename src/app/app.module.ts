2898427f3db34356e157c040c831a8a719509919
Artem Korobov <tyoma.end@gmail.com>
2021-02-08 14:20:19 +0300

    Создана разметка и часть стилей для кредитного калькулятора.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import {HeaderComponent} from './header/header.component';
 import {ColorBarComponent} from './widgets/color-bar/color-bar.component';
 import {FormsModule} from '@angular/forms';
 import { SocialMediaComponent } from './social-media/social-media.component';
+import { CreditCalculatorComponent } from './credit-calculator/credit-calculator.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { SocialMediaComponent } from './social-media/social-media.component';
     HeaderComponent,
     ColorBarComponent,
     SocialMediaComponent,
+    CreditCalculatorComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/credit-calculator/credit-calculator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-credit-calculator',
+  templateUrl: './credit-calculator.component.html',
+  styleUrls: ['./credit-calculator.component.scss']
+})
+export class CreditCalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
